e867736fc71f386481239b5e2df0d1b6b1369e2d
munir-hossin <hm.munirul.islam2222@gmail.com>
2024-10-11 00:48:54 +0600

    web development

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          'custom': ['"Azeret Mono"', 'sans-serif'], // or 'serif', 'monospace', etc.
+        },
+      },
+    },
+    // other configurations...
+  }
+  
+  // tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'custom-image': "url('../assets/images/main.png')", // Update the path as necessary
+
+        },
+      },
+    },
+    // other configurations...
+  }
+  
